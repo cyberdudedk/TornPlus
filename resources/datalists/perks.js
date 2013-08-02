344a2cf10c443165e2fb405b7e80c78a2af9f29f
cyberdudedk <perbellfield(at)gmail.com>
2013-08-03 00:26:20 +0200

    Yet again more datalists
    Perks, Ranks, Titles

---
+++ resources/datalists/perks.js
@@ -0,0 +1,594 @@
+var perksDatalist = {
+    'x Hospital time': {
+        type: 'Faction',
+        sign: '−'
+    },
+    'Crime Experience': {
+        type: 'Faction',
+        sign: '+'
+    },
+    'x% Gym Gains': {
+        type: 'Faction',
+        sign: '+'
+    },
+    'x Nerve': {
+        type: 'Faction',
+        sign: '+'
+    },
+    'x Travel Items': {
+        type: 'Faction',
+        sign: '+'
+    },
+    'x Reviving Energy Cost': {
+        type: 'Faction',
+        sign: '−'
+    },
+    'x Busting Energy Cost': {
+        type: 'Faction',
+        sign: '−'
+    },
+    'x% Education Length': {
+        type: 'Faction',
+        sign: '−'
+    },
+    'x% Drug Addiction Gain': {
+        type: 'Faction',
+        sign: '−'
+    },
+    'Ability to Revive': {
+        type: 'Job',
+        sign: '+'
+    },
+    '10% Education Time': {
+        type: 'Job',
+        sign: '−'
+    },
+    '10% Crime Success': {
+        type: 'Job',
+        sign: '+'
+    },
+    'Access to Airstrip': {
+        type: 'Property',
+        sign: '+'
+    },
+    '50% Life Regeneration': {
+        type: 'Property',
+        sign: '+'
+    },
+    '2% Damage': {
+        type: 'Property',
+        sign: '+'
+    },
+    '2% Gym Gains': {
+        type: 'Property',
+        sign: '+'
+    },
+    'Bank Interest Rate': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Firewall Protection': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Lawyer Cards': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional IOU Dividends': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional GRN Dividends': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Medical Packs': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Free Banner Placements': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Free Personal Placements': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Free Classified Placements': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional TMI Dividends': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Company Profit Boost': {
+        type: 'Stock',
+        sign: '+'
+    },
+    '50% Faster Virus Coding': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Hotel Coupons': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Drug Packs': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Lottery Tickets': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Erotic DVDs': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Grenade Packs': {
+        type: 'Stock',
+        sign: '+'
+    },
+    '25% Car Part Cost': {
+        type: 'Stock',
+        sign: '−'
+    },
+    '10% Property Upgrade Cost': {
+        type: 'Stock',
+        sign: '−'
+    },
+    'Occasional Free Properties': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Free Company Advertising': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'No Education Energy Cost': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'No Education Money Cost': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional Energy Boost': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Faster Travelling': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Occasional TCC Dividends': {
+        type: 'Stock',
+        sign: '+'
+    },
+    'Awareness': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'x% Crime Success': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'x Maximum Nerve': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'x% Critical Hit Rate': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Bank Interest Rate': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Strength': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Speed': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Dexterity': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Defense': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'x% life': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Mug Amount': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Stealth': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'x Hospitalizing Time': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Education Length': {
+        type: 'Merit',
+        sign: '−'
+    },
+    'Heavy Artillery Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Machine Gun Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Rifle Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'SMG Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Shotgun Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Pistol Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Club Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Piercing Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Slashing Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Mechanical Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'Temporary Dmg/Acc': {
+        type: 'Merit',
+        sign: '+'
+    },
+    'x% Strength': {
+        type: 'Education',
+        sign: '+'
+    },
+    'x% Speed': {
+        type: 'Education',
+        sign: '+'
+    },
+    'x% Dexterity': {
+        type: 'Education',
+        sign: '+'
+    },
+    'x% Defense': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Damage': {
+        type: 'Education',
+        sign: '+'
+    },
+    '10% Neck Hit Damage': {
+        type: 'Education',
+        sign: '+'
+    },
+    '25% Opponent Stealth': {
+        type: 'Education',
+        sign: '−'
+    },
+    'x% Medical Item Effectiveness': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Critical Hit Chance': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Medical Booster Equipping Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    '8% Company Productivity': {
+        type: 'Education',
+        sign: '+'
+    },
+    'x% Employee Effectiveness': {
+        type: 'Education',
+        sign: '+'
+    },
+    '15% Increase Prices Ability': {
+        type: 'Education',
+        sign: '+'
+    },
+    '15% Company Advertising Effectiveness': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Company Popularity': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Employee Working Stat': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Machine Gun Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% SMG Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Pistol Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Rifle Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Heavy Artillery Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Shotgun Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Dual Melee Weaponry Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Weapon Experience Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Coding Of Simple Virus Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Coding Of Polymorphic And Tunneling Virus Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Hacking Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Coding Of Armored And Stealth Virus Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    '20% Virus Coding Time': {
+        type: 'Education',
+        sign: '−'
+    },
+    '10% Hacking Success Rate': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Anonymous Mailing Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Driving Crimes Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Temporary Weapon Accuracy': {
+        type: 'Education',
+        sign: '+'
+    },
+    '5% Temporary Weapon Damage': {
+        type: 'Education',
+        sign: '+'
+    },
+    '15% Hunting Bonus': {
+        type: 'Education',
+        sign: '+'
+    },
+    '10% Working Stat Education Gain': {
+        type: 'Education',
+        sign: '+'
+    },
+    '50% Opponent Escape Chance': {
+        type: 'Education',
+        sign: '−'
+    },
+    '10% Japanese Blade Damage': {
+        type: 'Education',
+        sign: '+'
+    },
+    '2% Melee Damage': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Ability To Find Medieval Coins': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Ability To Find Sculptures And Quran Pages': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Ability To Find Senet Board And Amulets': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Museum Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Self Bailing Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Jail Escape Chance': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Escape Jail Nerve Cost': {
+        type: 'Education',
+        sign: '−'
+    },
+    'x% Busting Skill': {
+        type: 'Education',
+        sign: '+'
+    },
+    'x% Bail Cost': {
+        type: 'Education',
+        sign: '−'
+    },
+    '5% Estate Agent Cost': {
+        type: 'Education',
+        sign: '−'
+    },
+    '25% Company Advertising Cost': {
+        type: 'Education',
+        sign: '−'
+    },
+    'x% Ammo Management': {
+        type: 'Education',
+        sign: '+'
+    },
+    '10% Awareness': {
+        type: 'Education',
+        sign: '+'
+    },
+    '10% Crime Success Rate': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Kick Attack Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    '100% Fist Damage': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Strength Gym Gains': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Speed Gym Gains': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Defense Gym Gains': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Dexterity Gym Gains': {
+        type: 'Education',
+        sign: '+'
+    },
+    '10% Medical Booster Effectiveness': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Sports Shop Unlocked': {
+        type: 'Education',
+        sign: '+'
+    },
+    '1% Gym Gains': {
+        type: 'Education',
+        sign: '+'
+    },
+    'Arson success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Search for cash success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Bootlegging success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Shoplifting success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Pickpocketing success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Larceny success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Armed robbery success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Transport drugs success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Computer virus success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Assassination success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Grand theft auto success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Pawn shop success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Counterfeiting success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Kidnapping success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Arms Trafficking success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Bombing success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'Hacking success rate': {
+        type: 'Enhancer',
+        sign: '+'
+    },
+    'x Travel Items': {
+        type: 'Enhancer',
+        sign: '+'
+    }
+}
